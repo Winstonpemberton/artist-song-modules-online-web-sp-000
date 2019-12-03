30c16d099f07b1345448daece506749ad49afae1
Winston Pemberton <winstonpemberton@gmail.com>
2019-12-03 22:56:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,8 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
+  extend Memorable::ClassMethods
+
   @@songs = []
 
   def initialize
@@ -17,14 +19,6 @@ class Song
   def self.all
     @@songs
   end
-
-  # def self.reset_all
-  #   self.all.clear
-  # end
-  #
-  # def self.count
-  #   self.all.count
-  # end
 
   def artist=(artist)
     @artist = artist
